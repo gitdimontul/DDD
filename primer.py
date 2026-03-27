ad6244e1ada66760a5282e678c6776353b0c1b5d
gitdimontul <python.don@mail.ru>
2026-03-28 01:06:04 +0300

    new commit

--- primer.py
+++ primer.py
@@ -9,6 +9,7 @@ radius = 100
 arcade.open_window(width, height, title)
 arcade.set_background_color(arcade.color.WHITE)
 
+
 arcade.start_render()
 
 arcade.draw_circle_filled(width / 2, height / 2, radius, arcade.color.BLUE)
